7a19b3f27e012e258911d58c22edb23f33e5aacd
thiwakorn <thiwakorn.srimo@gmail.com>
2023-07-14 12:50:15 +0700

    add push noti cloud func

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,30 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/message_input.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void _setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    await fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    _setupPushNotifications();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
